449a76e179ce45a7f8277d8b4d08f4be7d447d49
ParfenenkovEdit <parfenenkovedit@gmail.com>
2020-12-11 13:58:13 +0300

    feat: add flying search/ refactor fetch data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import './styles/main.scss';
-import { getCovidData, setData, getCovidDataByCountry } from './scripts/statistics';
+import { setData, setCountryData } from './scripts/statistics';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  await getCovidData();
-  await getCovidDataByCountry('belarus');
   await setData();
+
+  document.getElementById('country-data-select').addEventListener('change', async (event) => {
+    await setCountryData(event.target.value, 'country-data');
+  });
+
+  document.getElementById('search').addEventListener('focus', () => document.querySelector('.search-data').setAttribute('data-show', ''));
+  document.getElementById('search').addEventListener('input', (event) => {
+    let value = event.target.value.toLowerCase();
+    document.querySelectorAll('.search-data-result').forEach((item) => {
+      if (item.textContent.toLowerCase().search(value) === -1) {
+        item.setAttribute('data-hide', '');
+      } else {
+        item.removeAttribute('data-hide');
+      }
+    });
+  });
+
+  document.querySelector('.search-data').addEventListener('click', async (event) => {
+    console.log(event.target);
+    if (event.target.classList.contains('search-data-result')) {
+      const country = event.target.getAttribute('data-country');
+      console.log(country);
+      await setCountryData(country, 'country-data');
+      document.querySelectorAll('.country-option').forEach((item) => {
+        if (item.value === country) {
+          item.setAttribute('selected', '');
+        }
+      });
+      document.querySelector('.search-data').removeAttribute('data-show');
+      document.getElementById('search').blur();
+    }
+  });
 });
--- src/scripts/statistics.js
+++ src/scripts/statistics.js
@@ -1,12 +1,12 @@
 import { DateTime } from 'luxon';
 
-async function getCovidData() {
+async function getData() {
   const response = await fetch('https://api.covid19api.com/summary');
   const data = await response.json();
   return data;
 }
 
-async function getCovidDataByCountry(country) {
+async function getDataByCountry(country) {
   const today = DateTime.utc().set({
     hour: 0,
     minute: 0,
@@ -22,45 +22,80 @@ async function getCovidDataByCountry(country) {
   return data[0];
 }
 
-async function setData() {
-  const data = await getCovidData();
-  const global = data.Global;
-  const countries = data.Countries;
-  const countriesCasesFragment = document.createDocumentFragment();
-  const countriesDeathFragment = document.createDocumentFragment();
-  const countriesRecoveredFragment = document.createDocumentFragment();
-
+function setCountriesData(data, value, selectorId) {
+  const fragment = document.createDocumentFragment();
+  const countries = data.sort((a, b) => b[value] - a[value]);
   countries.forEach((item) => {
-    const casesBlock = document.createElement('div');
-    const deathBlock = document.createElement('div');
-    const recoveredBlock = document.createElement('div');
+    const block = document.createElement('div');
 
-    casesBlock.classList.add('country-case');
-    casesBlock.innerHTML = `
-        <span class="dangerous">${item.TotalConfirmed}</span>
+    block.innerHTML = `
+        <span class="dangerous">${item[value]}</span>
         <span class="country-name">${item.Country}</span>
         `;
-
-    deathBlock.innerHTML = `
-      <span class="dangerous">${item.TotalDeaths}</span>
-      <span class="country-name">${item.Country}</span>
-      `;
-    recoveredBlock.innerHTML = `
-      <span class="dangerous">${item.TotalConfirmed}</span>
-      <span class="country-name">${item.Country}</span>
-      `;
-
-    countriesCasesFragment.appendChild(casesBlock);
-    countriesDeathFragment.appendChild(deathBlock);
-    countriesRecoveredFragment.appendChild(recoveredBlock);
+    fragment.appendChild(block);
   });
 
-  document.getElementById('global-cases').innerHTML = global.TotalConfirmed;
-  document.getElementById('countries-cases').appendChild(countriesCasesFragment);
-  document.getElementById('death-data').appendChild(countriesDeathFragment);
-  document.getElementById('recovered-data').appendChild(countriesRecoveredFragment);
+  document.getElementById(selectorId).appendChild(fragment);
 }
 
-export { getCovidData,
+function setCountrySelectList(data, selectorId) {
+  const select = document.createElement('select');
+  select.classList.add('select-country');
+
+  data.forEach((item) => {
+    const option = document.createElement('option');
+    option.classList.add('country-option');
+    option.textContent = item.Country;
+    option.value = item.Country;
+    select.appendChild(option);
+  });
+
+  document.getElementById(selectorId).prepend(select);
+}
+
+function setSearchCountryData(data, selectorId) {
+  const block = document.createElement('div');
+  block.classList.add('search-data');
+
+  data.forEach((item) => {
+    const dataBlock = document.createElement('div');
+    dataBlock.classList.add('search-data-result');
+    dataBlock.textContent = item.Country;
+    dataBlock.setAttribute('data-country', item.Country);
+    block.appendChild(dataBlock);
+  });
+
+  document.getElementById(selectorId).appendChild(block);
+}
+
+async function setData() {
+  const data = await getData();
+  const global = data.Global;
+  const countries = data.Countries;
+  setCountriesData(countries, 'TotalConfirmed', 'countries-cases');
+  setCountriesData(countries, 'TotalDeaths', 'death-data');
+  setCountriesData(countries, 'TotalRecovered', 'recovered-data');
+  setCountrySelectList(countries, 'country-data-select');
+  setSearchCountryData(countries, 'search-section');
+
+  document.getElementById('global-cases').innerHTML = global.TotalConfirmed;
+}
+
+async function setCountryData(value, selectorId) {
+  const data = await getDataByCountry(value);
+  const activeSick = data.Active;
+  const confirmedSick = data.Confirmed;
+  const deathSick = data.Deaths;
+  const recoveredSick = data.Recovered;
+
+  document.getElementById(selectorId).innerHTML = `
+      <h2>Active: ${activeSick}</h2>
+      <h2>Confirmed: ${confirmedSick}</h2>
+      <h2>Deaths: ${deathSick}</h2>
+      <h2>Recovered: ${recoveredSick}</h2>
+  `;
+}
+
+export { getData,
   setData,
-  getCovidDataByCountry };
+  setCountryData };
