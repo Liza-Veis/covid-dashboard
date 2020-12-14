7d4369fbf883be952baf10534fed53c8a530a50a
Liza Veis <liza.veis14@gmail.com>
2020-12-14 23:51:23 +0200

    feat: implement Interactive-Map class

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,17 +1,69 @@
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import { map } from './markup';
+// import { map } from './markup';
 
-const LOCATION = {
-  lat: 15,
-  lng: 0
-};
-const CENTER = [LOCATION.lat, LOCATION.lng];
+const CENTER = [15, 0];
 const DEFAULT_ZOOM = 2;
 const MIN_ZOOM = 2;
 
-function interactiveMap() {
-  function countryPointToLayer(feature = {}, latlng) {
+const SOUTH_WEST = L.latLng(-80, -250);
+const NORTH_EAST = L.latLng(90, 250);
+const BOUNDS = L.latLngBounds(SOUTH_WEST, NORTH_EAST);
+
+const mapSettings = {
+  center: CENTER,
+  defaultBaseMap: 'OpenStreetMap',
+  zoom: DEFAULT_ZOOM,
+  minZoom: MIN_ZOOM,
+  maxBounds: BOUNDS
+};
+
+class InteractiveMap {
+  constructor(mapContainer) {
+    this.map = new L.map(mapContainer, mapSettings);
+
+    this.map.on('drag', () => {
+      this.map.panInsideBounds(BOUNDS, { animate: false });
+    });
+
+    const layer = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    this.addPointsLayer(this.map);
+    this.map.addLayer(layer);
+  }
+
+  async getData() {
+    const data = await fetch('https://corona.lmao.ninja/v2/countries').then((res) => res.json());
+
+    return data;
+  }
+
+  createJeoLayer(data) {
+    const geoJson = {
+      type: 'FeaturesCollection',
+      features: data.map((country = {}) => {
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
+        return {
+          type: 'Feature',
+          properties: {
+            ...country
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [lng, lat]
+          }
+        };
+      })
+    };
+
+    const geoLayer = L.geoJSON(geoJson, {
+      pointToLayer: this.countryPointToLayer
+    });
+
+    geoLayer.addTo(this.map);
+  }
+
+  countryPointToLayer(feature = {}, latlng) {
     const { properties = {} } = feature;
     let updatedFormatted;
     let casesString;
@@ -29,19 +81,18 @@ function interactiveMap() {
     }
 
     const html = `
-  <span class="icon-marker">
-  <span class="icon-marker-tooltip">
-  <h2>${country}</h2>
-  <ul>
-  <li><strong>Confirmed:</strong>${cases}</li>
-  <li><strong>Deaths:</strong>${deaths}</li>
-  <li><strong>Recovered:</strong>${recovered}</li>
-  <li><strong>Last Update:</strong>${updatedFormatted}</li>
-  </ul>
-  </span>
-  ${casesString}
-  </span>
-  `;
+ 	<span class="icon-marker">
+ 	<span class="icon-marker-tooltip">
+ 	<h2>${country}</h2>
+ 	<ul>
+ 	<li><strong>Confirmed:</strong>${cases}</li>
+ 	<li><strong>Deaths:</strong>${deaths}</li>
+ 	<li><strong>Recovered:</strong>${recovered}</li>
+ 	<li><strong>Last Update:</strong>${updatedFormatted}</li>
+ 	</ul>
+ 	</span>
+ 	${casesString}
+ 	</span>`;
 
     return L.marker(latlng, {
       icon: L.divIcon({
@@ -52,63 +103,16 @@ function interactiveMap() {
     });
   }
 
-  async function mapEffect(map) {
-    //   if (!map) return;
-    const createJeo = (data) => {
-      const geoJson = {
-        type: 'FeaturesCollection',
-        features: data.map((country = {}) => {
-          const { countryInfo = {} } = country;
-          const { lat, long: lng } = countryInfo;
-          return {
-            type: 'Feature',
-            properties: {
-              ...country
-            },
-            geometry: {
-              type: 'Point',
-              coordinates: [lng, lat]
-            }
-          };
-        })
-      };
+  async addPointsLayer(map) {
+    if (!map) return;
 
-      const geoJsonLayers = L.geoJSON(geoJson, {
-        pointToLayer: countryPointToLayer
-      });
-      geoJsonLayers.addTo(map);
-    };
-
-    await fetch('https://corona.lmao.ninja/v2/countries')
-      .then((response) => response.json())
-      .then(createJeo)
+    this.getData()
+      .then(this.createJeoLayer.bind(this))
       .catch((e) => {
         console.log('E: ', e);
         return;
       });
   }
-
-  const southWest = L.latLng(-80, -250);
-  const northEast = L.latLng(90, 250);
-  const bounds = L.latLngBounds(southWest, northEast);
-
-  const mapSettings = {
-    center: CENTER,
-    defaultBaseMap: 'OpenStreetMap',
-    zoom: DEFAULT_ZOOM,
-    minZoom: MIN_ZOOM,
-    maxBounds: bounds
-  };
-
-  const myMap = new L.map(map, mapSettings);
-
-  myMap.on('drag', function () {
-    myMap.panInsideBounds(bounds, { animate: false });
-  });
-
-  const layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-  mapEffect(myMap);
-  myMap.addLayer(layer);
 }
 
-export default interactiveMap;
+export default InteractiveMap;
